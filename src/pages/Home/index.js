a6c140d2c7f846343d2e5d15ef0b666d9a7518d8
danubiobwm <danubio.bwm@gmail.com>
2019-09-10 00:40:57 -0300

    Home Stylization

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,95 @@
 import React from 'react';
+import {MdAddShoppingCart} from 'react-icons/md';
 
-// import { Container } from './styles';
+import {ProductList} from './styles';
 
 export default function Home() {
-  return <h1>home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7EsqgpvOo_X7h9Tg5wvbo-J9GA1dlWMHPt4D-9g5CSjE25dC5vcttJHVttQ0f8fbOcEaWjqfyj6OrpRLr_ZZalGcA2X6oLYq5OFSP08o4mexP7Voe_tqtJg&usqp=CAE"
+          alt="Tenis"
+        />
+        <strong>Tênis com desconto 50%</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7EsqgpvOo_X7h9Tg5wvbo-J9GA1dlWMHPt4D-9g5CSjE25dC5vcttJHVttQ0f8fbOcEaWjqfyj6OrpRLr_ZZalGcA2X6oLYq5OFSP08o4mexP7Voe_tqtJg&usqp=CAE"
+          alt="Tenis"
+        />
+        <strong>Tênis com desconto 50%</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7EsqgpvOo_X7h9Tg5wvbo-J9GA1dlWMHPt4D-9g5CSjE25dC5vcttJHVttQ0f8fbOcEaWjqfyj6OrpRLr_ZZalGcA2X6oLYq5OFSP08o4mexP7Voe_tqtJg&usqp=CAE"
+          alt="Tenis"
+        />
+        <strong>Tênis com desconto 50%</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7EsqgpvOo_X7h9Tg5wvbo-J9GA1dlWMHPt4D-9g5CSjE25dC5vcttJHVttQ0f8fbOcEaWjqfyj6OrpRLr_ZZalGcA2X6oLYq5OFSP08o4mexP7Voe_tqtJg&usqp=CAE"
+          alt="Tenis"
+        />
+        <strong>Tênis com desconto 50%</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7EsqgpvOo_X7h9Tg5wvbo-J9GA1dlWMHPt4D-9g5CSjE25dC5vcttJHVttQ0f8fbOcEaWjqfyj6OrpRLr_ZZalGcA2X6oLYq5OFSP08o4mexP7Voe_tqtJg&usqp=CAE"
+          alt="Tenis"
+        />
+        <strong>Tênis com desconto 50%</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7EsqgpvOo_X7h9Tg5wvbo-J9GA1dlWMHPt4D-9g5CSjE25dC5vcttJHVttQ0f8fbOcEaWjqfyj6OrpRLr_ZZalGcA2X6oLYq5OFSP08o4mexP7Voe_tqtJg&usqp=CAE"
+          alt="Tenis"
+        />
+        <strong>Tênis com desconto 50%</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
