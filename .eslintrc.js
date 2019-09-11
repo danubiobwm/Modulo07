e44c0c0a520052a3b4939e8edce82b233a8026f7
danubiobwm <danubio.bwm@gmail.com>
2019-09-11 17:02:18 -0300

    Reactotron + Redux

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,7 @@ module.exports = {
       'warn',
       { extensions: ['.jsx', '.js'] }
     ],
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'no-console':["error", {allow:["tron"]}]
   },
 }
