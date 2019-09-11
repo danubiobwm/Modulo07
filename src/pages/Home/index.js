e7bcd7facc13482ab2263039322bb1509377212a
danubiobwm <danubio.bwm@gmail.com>
2019-09-10 23:47:08 -0300

    Searching API Products

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,95 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {MdAddShoppingCart} from 'react-icons/md';
-
+import {formatPrice} from '../../util/format';
+import api from '../../services/api';
 import {ProductList} from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7EsqgpvOo_X7h9Tg5wvbo-J9GA1dlWMHPt4D-9g5CSjE25dC5vcttJHVttQ0f8fbOcEaWjqfyj6OrpRLr_ZZalGcA2X6oLYq5OFSP08o4mexP7Voe_tqtJg&usqp=CAE"
-          alt="Tenis"
-        />
-        <strong>Tênis com desconto 50%</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7EsqgpvOo_X7h9Tg5wvbo-J9GA1dlWMHPt4D-9g5CSjE25dC5vcttJHVttQ0f8fbOcEaWjqfyj6OrpRLr_ZZalGcA2X6oLYq5OFSP08o4mexP7Voe_tqtJg&usqp=CAE"
-          alt="Tenis"
-        />
-        <strong>Tênis com desconto 50%</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7EsqgpvOo_X7h9Tg5wvbo-J9GA1dlWMHPt4D-9g5CSjE25dC5vcttJHVttQ0f8fbOcEaWjqfyj6OrpRLr_ZZalGcA2X6oLYq5OFSP08o4mexP7Voe_tqtJg&usqp=CAE"
-          alt="Tenis"
-        />
-        <strong>Tênis com desconto 50%</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7EsqgpvOo_X7h9Tg5wvbo-J9GA1dlWMHPt4D-9g5CSjE25dC5vcttJHVttQ0f8fbOcEaWjqfyj6OrpRLr_ZZalGcA2X6oLYq5OFSP08o4mexP7Voe_tqtJg&usqp=CAE"
-          alt="Tenis"
-        />
-        <strong>Tênis com desconto 50%</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7EsqgpvOo_X7h9Tg5wvbo-J9GA1dlWMHPt4D-9g5CSjE25dC5vcttJHVttQ0f8fbOcEaWjqfyj6OrpRLr_ZZalGcA2X6oLYq5OFSP08o4mexP7Voe_tqtJg&usqp=CAE"
-          alt="Tenis"
-        />
-        <strong>Tênis com desconto 50%</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT7EsqgpvOo_X7h9Tg5wvbo-J9GA1dlWMHPt4D-9g5CSjE25dC5vcttJHVttQ0f8fbOcEaWjqfyj6OrpRLr_ZZalGcA2X6oLYq5OFSP08o4mexP7Voe_tqtJg&usqp=CAE"
-          alt="Tenis"
-        />
-        <strong>Tênis com desconto 50%</strong>
-        <span>R$129,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({products: data});
+  }
+
+  render() {
+    const {products} = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
